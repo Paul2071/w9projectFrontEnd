97b6d1e643c2aea601513dcb12665d310a72b0b0
JackC91 <jackjcherry@hotmail.co.uk>
2022-06-22 11:24:05 +0100

    inital function to connect backend written and tested, awaiting refactor

--- team-12-react-app/src/components/MainCard/index.js
+++ team-12-react-app/src/components/MainCard/index.js
@@ -18,13 +18,28 @@ Code
 import React from "react";
 import Input from "../Input";
 
+/*
+run front and back end servers
+import useState + useEffect from React
+write fetch request to get all route on server
+use useEffect to call fetch function when page loads
+*/
+
+async function fetchAPI() {
+  const response = await fetch("http://localhost:3005/computational_thinking");
+  const data = await response.json();
+  console.log(data.payload.rows);
+}
+
+
 
 function MainCard() {
   return (
     <div className="maincard--container">
       <h3>MainCard here</h3>
      <Input ></Input>
-  
+     <button onClick={() => {fetchAPI()}}>Click</button>
+      
     </div>
   );
 }

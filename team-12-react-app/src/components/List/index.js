f0363583a955b66fcf6cde61c2046ebbac6166ea
ONESSR <ssr677@gmail.com>
2022-06-23 16:28:27 +0100

    CSS for demo complete. Next: Clean up CSS functionality.

--- team-12-react-app/src/components/List/index.js
+++ team-12-react-app/src/components/List/index.js
@@ -27,48 +27,43 @@ Code
         -  EditButton considered but deleted from App functionality for now. Will be added   if requirements change.
 */
 
-
 import React from "react";
-import "../App/App.css"
+import "../App/App.css";
 
-
-
-function ListItem({tasks, fetchAPI}){
-
+function ListItem({ tasks, fetchAPI }) {
   // event.target.style.textDecoration
   //className={toggleClass ? "strike" : "nonstrike"} onClick={handleToggle}
 
-//   function handleToggle (event, {tasks}) {
-//   if (tasks.is_complete === false) {
-//     event.target.style.removeProperty('text-decoration');
-//   } else {
-//     event.target.style.setProperty('text-decoration', 'line-through');
-//   }
-// }
+  //   function handleToggle (event, {tasks}) {
+  //   if (tasks.is_complete === false) {
+  //     event.target.style.removeProperty('text-decoration');
+  //   } else {
+  //     event.target.style.setProperty('text-decoration', 'line-through');
+  //   }
+  // }
 
-//function to change boolean value of is_completed column in DB
-function updateStatus(id) {
-  async function updateAPI() {
-    await fetch("http://localhost:3005/user_table",
-    {method: "PATCH",
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({task_id: id})
-  })
+  //function to change boolean value of is_completed column in DB
+  function updateStatus(id) {
+    async function updateAPI() {
+      await fetch("http://localhost:3005/user_table", {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ task_id: id })
+      });
+    }
+    fetchAPI();
+    updateAPI();
+    fetchAPI();
   }
-  fetchAPI();
-  updateAPI();
-  fetchAPI();
-}
 
   //function that fires when delete button clicked
   function deleteClick(id) {
-    
     async function deleteAPI() {
-      await fetch("http://localhost:3005/user_table",
-      {method: "DELETE",
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({task_id: id})
-    })
+      await fetch("http://localhost:3005/user_table", {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ task_id: id })
+      });
     }
     fetchAPI();
     deleteAPI();
@@ -79,22 +74,35 @@ function updateStatus(id) {
     <ul id="list--container">
       {tasks.map(function (item) {
         return (
-          <li key={item.task_id}>
-            {item.task}
-            <button
-              onClick={() => {
-                deleteClick(item.task_id);
-              }}
-              className="delete--button"
+          <li key={item.task_id} className="li-item">
+            <label class="container">
+              <input
+                className="checkbox"
+                type="checkbox"
+                onClick={() => {
+                  updateStatus(item.task_id);
+                }}
+              />
+              <span class="checkmark"></span>
+            </label>
+
+            <div className="li-input-text">{item.task}</div>
+
+            <div>
+              <button
+                onClick={() => {
+                  deleteClick(item.task_id);
+                }}
+                className="delete--button"
               >
-              Remove
-            </button>
-            <input type="checkbox" onClick={() => {updateStatus(item.task_id)}}/>
+                X
+              </button>
+            </div>
           </li>
         );
       })}
     </ul>
-  )
+  );
 }
 
 export default ListItem;

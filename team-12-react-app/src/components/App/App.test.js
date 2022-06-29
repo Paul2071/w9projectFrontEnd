b274f6d3b647e5783001687830b57dd665eadfe4
halil-coskun <halilcoskun14@gmail.com>
2022-06-29 12:05:30 +0100

    refactored existing tests

--- team-12-react-app/src/components/App/App.test.js
+++ team-12-react-app/src/components/App/App.test.js
@@ -1,16 +1,12 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import {render, screen } from '@testing-library/react';
 import App from './App';
 
-//test 1: check that the page loads correctly by doing a get by text
-//test 2: check that add to list button runs a mock function 
-//test 3: 
+test.only('renders the app and checks that button is present for every maincard in the page', () => {
+  
+render(<App />);
 
-test('renders app and check week that main card is rendered', () => {
-  //Arrange
-  render(<App />);
-  //Act
- const mainCard = screen.getByText("MainCard here");
- // Assert that the card is in the document
- expect(mainCard).toBeInTheDocument();
+const allButtons = screen.getAllByRole ("button");
+
+expect.arrayContaining(allButtons)
+
 });
-
--- team-12-react-app/src/components/Input/Input.test.js
+++ team-12-react-app/src/components/Input/Input.test.js
@@ -1,40 +1,32 @@
-import React from 'react'
-import {render, screen} from '@testing-library/react'
-import '@testing-library/jest-dom'
-import userEvent from "@testing-library/user-event"
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
-// TEST 1
-test('renders button and checks it is present on page', () => {
-    //Arrange - render thebutton
-    render(<button />);
-    //Act
-   const button = screen.getByRole("button");
-   // Assert that the button is in the document
-   expect(button).toBeInTheDocument();
-  });
+test("renders button and checks it is present on page", () => {
+  render(<button />);
 
-// TEST 2
-test('checks that input field has correct placeholder text', () => {
-    //Arrange - render the input field and define the text it should have
-    const text = "Click list item to complete"
-    render(<input placeholder='Click list item to complete'/>);
-    //Act
-   const inputfield = screen.getByPlaceholderText(text);
-   // Assert that the input is in the document
-   expect(inputfield).toBeInTheDocument()
-  });
+  const button = screen.getByRole("button");
 
-//TEST 3
-test('check that add to list buttons runs a mock function', () => {
-    //Arrange - use the mock function jest.fn
-    const mock = jest.fn()
+  expect(button).toBeInTheDocument();
+});
 
-    render(<button onClick={mock} />);
-    const button = screen.getByRole("button");
+test("checks that input field has correct placeholder text", () => {
+  const text = "Add tasks here...";
+  render(<input placeholder="Add tasks here..." />);
 
-    //Act - user clicks button
-    userEvent.click(button)
-    //Assert - make sure that a mock function has been called onClick of our button
-    expect(mock).toHaveBeenCalled()
-  });
-  
+  const inputfield = screen.getByPlaceholderText(text);
+
+  expect(inputfield).toBeInTheDocument();
+});
+
+test("check that add to list buttons runs a mock function", () => {
+  const mock = jest.fn();
+
+  render(<button onClick={mock} />);
+  const button = screen.getByRole("button");
+
+  userEvent.click(button);
+
+  expect(mock).toHaveBeenCalled();
+});
